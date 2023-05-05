02485ade69a24567ee54d46d8a56cb8d1b8846ee
pabloperguz <p.perez-guzman@imperial.ac.uk>
2023-05-05 13:46:01 +0100

    process historic deaths in data task

--- src/ZamCovid_data/script.R
+++ src/ZamCovid_data/script.R
@@ -7,8 +7,7 @@ end_date <- as.Date("2021-12-31")
 deaths <- read_csv("data/kabwe_all_deaths.csv") %>% mutate(date = as.Date(date))
 serology <- read_csv("data/kabwe_seroprevalence.csv") %>% mutate(date = as.Date(date))
 cases <- read_csv("data/kabwe_cases.csv") %>% mutate(date = as.Date(date))
-historic_deaths <- read_csv("data/kabwe_historic_deaths.csv") %>%
-  mutate(month = factor(month, levels = month.abb))
+historic_deaths <- get_baseline_deaths()
 
 ## Data will run from 2020-01-01 to 2021-12-31
 # Note that:
@@ -20,9 +19,13 @@ data <- data.frame(date = seq.Date(start_date, end_date, "day")) %>%
   left_join(., cases, by = "date") %>%
   left_join(., deaths, by = "date")
 
-write.csv(data, "data_timeseries.csv", row.names = FALSE)
+data <- list(
+  data = data,
+  historic = historic_deaths)
+
+saveRDS(data, "data_timeseries.rds")
 
 
 png("data_to_fit.png", units = "in", width = 14, height = 10, res = 300)
-plot_data(data)
+plot_data(data$data)
 dev.off()
