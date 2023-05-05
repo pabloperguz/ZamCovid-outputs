315edf02651bf0cded677bc6c34d85ad6fb93562
pabloperguz <p.perez-guzman@imperial.ac.uk>
2023-05-05 13:12:09 +0100

    output IFR(t) and IFR by age

--- src/ZamCovid_fits/script.R
+++ src/ZamCovid_fits/script.R
@@ -33,6 +33,10 @@ write_csv(dat$fit$parameters$proposal, "outputs/proposal.csv")
 dir.create("plots", FALSE, TRUE)
 message("Creating plots")
 
+png("plots/fits_ifr.png", units = "in", width = 6, height = 6, res = 300)
+plot_severity(dat)
+dev.off()
+
 png("plots/fits_rt.png", units = "in", width = 6, height = 6, res = 300)
 plot_rt(dat)
 dev.off()
