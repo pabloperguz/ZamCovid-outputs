536856c4f10c4a877036487cc4526f663276b5d0
pabloperguz <p.perez-guzman@imperial.ac.uk>
2023-05-10 12:29:47 +0100

    additional support functions

--- src/ZamCovid_parameters/R/support.R
+++ src/ZamCovid_parameters/R/support.R
@@ -112,3 +112,32 @@ prior_rename <- function (x, from, to, name = deparse(substitute(x))) {
   names(x)[i] <- to
   x
 }
+
+
+verify_names <- function (x, required = NULL, optional = NULL, allow_extra = FALSE, 
+                          name = deparse(substitute(x))) {
+  nms <- names(x)
+  if (anyDuplicated(nms)) {
+    dups <- unique(nms[duplicated(nms)])
+    stop(sprintf("Duplicate element names in '%s': %s", name, 
+                 paste(squote(dups), collapse = ", ")))
+  }
+  if (!allow_extra) {
+    extra <- setdiff(nms, c(required, optional))
+    if (length(extra) > 0) {
+      stop(sprintf("Extra elements in '%s': %s", name, 
+                   paste(squote(extra), collapse = ", ")))
+    }
+  }
+  msg <- setdiff(required, nms)
+  if (length(msg) > 0) {
+    stop(sprintf("Elements missing from '%s': %s", name, 
+                 paste(squote(msg), collapse = ", ")))
+  }
+  invisible(x)
+}
+
+
+squote <- function (x) {
+  sprintf("'%s'", x)
+}
