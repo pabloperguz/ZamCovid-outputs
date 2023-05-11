11ecd76d2a583560465780ed55688a8c28ef0daf
pabloperguz <p.perez-guzman@imperial.ac.uk>
2023-05-11 14:11:43 +0100

    try best fitting assumptions

--- cluster_scripts/ZamCovid_kabwe.R
+++ cluster_scripts/ZamCovid_kabwe.R
@@ -10,7 +10,7 @@ root_dir <- paste0(orderly::orderly_config()$root, "/src/")
 short_run <- TRUE
 date <- "2021-08-01"
 assumptions <- "serorev_slow"
-## can be: central, fit_no_deaths,
+## can be: central, best_fit,
 ##         base_deaths_high, base_deaths_low,
 ##         imm_waning_high, imm_waning_low,
 ##         p_G_D_high, p_G_D_low,
