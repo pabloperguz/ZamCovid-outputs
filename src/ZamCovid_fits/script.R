d624d18853e7766a709f000011ef1267516e8cd2
pabloperguz <p.perez-guzman@imperial.ac.uk>
2023-06-28 13:34:58 +0100

    plot cumulative infected by age

--- src/ZamCovid_fits/script.R
+++ src/ZamCovid_fits/script.R
@@ -68,8 +68,10 @@ dev.off()
 
 # Epidemics9 plot
 col1 <- (
-  (plot_serology(dat$fit$samples, data_full, over15_only = TRUE) +
-     theme(axis.text.x = element_blank())) /
+  (plot_serology(dat$fit$samples, data_full, which = "over15",
+                 labels = "15 and older") +
+     theme(legend.position = "right",
+           axis.text.x = element_blank())) /
     (plot_rt(dat) + theme(axis.text.x = element_blank())) /
     plot_infection_incidence(dat)) +
   plot_layout(heights = c(0.3, 0.3, 1))
